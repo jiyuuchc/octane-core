a29fa3f07719a8695dc6c0e8c117a7270452d5cf
Ji Yu <Ji Yu@10.84.12.36>
2017-09-13 09:55:33 -0400

    Fix issue due to MASKED tag and change to a better class name

--- src/edu/uchc/octanecore/frameanalysis/LocalMaximumProcessor.java
+++ src/edu/uchc/octanecore/frameanalysis/LocalMaximumProcessor.java
@@ -26,48 +26,48 @@ import org.apache.commons.math3.util.FastMath;
 
 import edu.uchc.octanecore.datasource.RectangularImage;
 
-public class WatershedProcessor{
+public class LocalMaximumProcessor{
 
 	public int ROISize;
-	public double threshold, noise;
-	
-	public interface CallBackFunctions {	
+	public double threshold, tolerance;
 
-		public boolean fit(RectangularImage ROI, int x, int y); 
-	
+	public interface CallBackFunctions {
+
+		public boolean fit(RectangularImage ROI, int x, int y);
+
 	}
-	
+
 	protected final static int PROCESSED = 1;
-	protected final static int FLOODED = 2;
-	protected final static int MASKED = 4;
+	protected final static int COLLECTED = 2;
+//	protected final static int MASKED = 4;
 
 	protected RectangularImage data;
 	protected char [] pixelStates;
-	
+
 	class Pixel implements Comparable<Pixel> {
-		
+
 		public int idx;
 
 		public Pixel(int idx) {
 			this.idx = idx;
 		}
-		
+
 		@Override
 		public int compareTo(Pixel o) {
-			
+
 			return (int) FastMath.signum(- data.getValue(idx) + data.getValue(o.idx));
-		
+
 		}
 	}
 
-	public WatershedProcessor(double noise) {
+	public LocalMaximumProcessor(double noise) {
 		this(noise, 0, 11);
 	}
 
-	public WatershedProcessor(double noise, int threshold, int ROISize) {
+	public LocalMaximumProcessor(double noise, int threshold, int ROISize) {
 
 		this.threshold = threshold;
-		this.noise = noise;
+		this.tolerance = noise;
 		this.ROISize = ROISize;
 	}
 
@@ -75,62 +75,62 @@ public class WatershedProcessor{
 		return ( ( pixelStates[index] & PROCESSED ) != 0);
 	}
 
-	public boolean isFlooded(int index) {
-		return ( ( pixelStates[index] & FLOODED) != 0);
+	public boolean isCollected(int index) {
+		return ( ( pixelStates[index] & COLLECTED) != 0);
 	}
 
-	public boolean isMasked(int index) {
-		return ( ( pixelStates[index] & MASKED) != 0);
-	}
+//	public boolean isMasked(int index) {
+//		return ( ( pixelStates[index] & MASKED) != 0);
+//	}
 
 	public void process(int index) {
 		pixelStates[index] |= PROCESSED;
 	}
 
-	public void flood(int index) {
-		pixelStates[index] |= FLOODED;
+	public void collect(int index) {
+		pixelStates[index] |= COLLECTED;
 	}
-	
-	public void mask(int index) {
-		pixelStates[index] |= MASKED;
-	}
-	
+
+//	public void mask(int index) {
+//		pixelStates[index] |= MASKED;
+//	}
+
 	public void processFrame(RectangularImage data, CallBackFunctions callback) {
-		
+
 		this.data = data;
 		pixelStates = new char[data.getLength()];
 
 		ArrayList<Pixel> pixels = new ArrayList<Pixel>();
 
 		for (int i = 0; i < data.getLength(); i ++) {
-			
+
 			if (data.getValue(i) > threshold) {
 				pixels.add(new Pixel(i));
 			} else {
-				mask(i);
+//				mask(i);
 			}
 		}
 		Collections.sort(pixels);
-		 
+
 		ArrayDeque<Integer> queue = new ArrayDeque<Integer>();
 		ArrayList<Integer> pool = new ArrayList<Integer>();
- 
+
 		for (Pixel pixel : pixels) {
-			
+
 			if ( isProcessed(pixel.idx) ){
 				continue;
 			}
 
 			double peakValue = data.getValue(pixel.idx);
 			boolean isMax = true;
-			
+
 			queue.clear();
 			pool.clear();
 
 			queue.offer(pixel.idx);
-			
-			flood(pixel.idx);
-			
+
+			collect(pixel.idx);
+
 			while (! queue.isEmpty()) {
 
 				int index = queue.poll();
@@ -140,20 +140,23 @@ public class WatershedProcessor{
 				for (int i = 0; i < neighbours.length; i++) { // analyze all neighbors (in 8 directions) at the same level
 
 					if ( isProcessed(neighbours[i]) ) { //conflict
-						isMax = false; 
+						isMax = false;
+						for (Integer idx : queue) {
+							process(idx);
+						}
 						break;
 					}
 
-					if ( ! isFlooded(neighbours[i]) && ! isMasked(neighbours[i]) && data.getValue(neighbours[i]) > peakValue - noise) {
+					if ( ! isCollected(neighbours[i]) && /*! isMasked(neighbours[i]) && */ data.getValue(neighbours[i]) > peakValue - tolerance) {
 						queue.add(neighbours[i]);
-						flood(neighbours[i]);
+						collect(neighbours[i]);
 					}
-				} 
+				}
 			}
 
 			for ( Integer idx : pool) {
 				process(idx);
-			} 
+			}
 
 			if (isMax) {
 
@@ -162,9 +165,9 @@ public class WatershedProcessor{
 				RectangularImage subImage = new RectangularImage(data, x - ROISize / 2, y - ROISize / 2, ROISize, ROISize, true);
 				if (callback.fit(subImage, x, y) == false) {
 					break;
-				}				
+				}
 			}
 		}
 	}
-	
+
 }
