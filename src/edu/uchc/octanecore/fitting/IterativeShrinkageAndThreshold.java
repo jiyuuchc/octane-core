0fdc447f0abf510d0840d5058dddfe1144d30199
Ji Yu <Ji Yu@10.84.12.36>
2017-09-14 16:52:37 -0400

    Add Fista fitting class

---
+++ src/edu/uchc/octanecore/fitting/IterativeShrinkageAndThreshold.java
@@ -0,0 +1,146 @@
+package edu.uchc.octanecore.fitting;
+
+import org.apache.commons.math3.util.FastMath;
+
+import edu.uchc.octanecore.datasource.ImageData;
+import edu.uchc.octanecore.datasource.RectangularImage;
+import edu.uchc.octanecore.utils.ImageFilters;
+
+public class IterativeShrinkageAndThreshold {
+
+    public final static int MAX_ITERATIONS = 50;
+    public final static double CONVERGENCE_DELTA = 1e-6;
+
+    public int maxIters;
+    public ImageData data;
+    public double psfSigma;
+    public int scaleFactor;
+    public double ts, lambda;
+
+    protected int imgWidth, imgHeight;
+    protected double [] x;
+    protected double [] y;
+    protected double [] h;
+    protected double t;
+
+    public double[][] fittedParameters;
+
+    double l2, l1, l0;
+    static public class Params {
+    	static final int X = 0;
+    	static final int Y = 1;
+    	static final int PARAMS_LENGTH = 2;
+    }
+
+    public IterativeShrinkageAndThreshold(double lambda) {
+    	this(lambda, 1.00 / 2 / FastMath.PI,  500);
+    }
+
+    public IterativeShrinkageAndThreshold(double lambda, double ts, int maxIters) {
+    	this.lambda = lambda;
+    	this.ts = ts;
+    	this.maxIters = maxIters;
+    }
+
+    public double[] fit(RectangularImage image, int scale, double sigma) {
+
+    	this.data = image;
+    	imgWidth = image.width * scale;
+    	imgHeight = image.height * scale;
+    	scaleFactor = scale;
+    	psfSigma = sigma;
+
+    	x = new double[imgWidth * imgHeight];
+
+//    	if (hint.length > 0) {
+//    		for (int i = 0; i < hint.length; i += 2) {
+//    			x[hint[i+1] * scale * imgWidth + hint[i] * scale] = 1;
+//    		}
+//
+//    	}
+
+    	y = x.clone();
+
+    	int filterSize = (int)(psfSigma * scaleFactor * 3 + 0.5) * 2 + 1;
+    	filterSize = FastMath.min(FastMath.min(imgWidth, imgHeight), filterSize);
+    	h = ImageFilters.makeGaussianFilter(psfSigma * scaleFactor, filterSize, true);
+
+    	t = 1.0;
+    	for (int i = 0; i < maxIters; i++) {
+
+			iterate();
+
+		}
+
+    	return x;
+    }
+
+	protected void iterate() {
+
+		double [] x0 = x.clone();
+		// Ay
+		double [] tmp = ImageFilters.symmetricFilter(h, y, imgWidth);
+
+		// tmp2 = Ay - b
+		//l2 = 0; l1=0; l0 = 0;
+		for (int i = 0; i < imgHeight; i ++) {
+			for (int j = 0; j < imgWidth; j ++) {
+				tmp[i * imgWidth + j] -= data.getValueAtCoordinate(j / scaleFactor, i / scaleFactor) / scaleFactor / scaleFactor;
+//				l2 += tmp[i * imgWidth + j] * tmp[i * imgWidth + j] * scaleFactor * scaleFactor;
+//				if (x[i * imgWidth + j] != 0.0) {
+//					l1 += lambda * x[i * imgWidth + j];
+//					l0 += 1;
+//				}
+			}
+		}
+
+		// tmp = A^T(Ay - b)
+		tmp = ImageFilters.symmetricFilter(h, tmp, imgWidth);
+
+		// x = pL()
+		for (int i = 0; i < tmp.length; i ++) {
+			x[i] = y[i] - 2.0 * ts * tmp[i];
+		}
+
+		// soft threshold
+//		System.out.println("X");
+		for (int i = 0; i < x.length; i ++) {
+
+			double tt = FastMath.abs(x[i]) - ts * lambda / scaleFactor / scaleFactor;
+
+			if (tt <= 0.0){
+
+				x[i] = 0;
+
+			}
+			else {
+
+				x[i] = FastMath.signum(x[i]) * tt;
+
+			}
+
+//			if (i % imgWidth == 0) {
+//				System.out.println();
+//			}
+//			System.out.print(x[i]);
+//			System.out.print(",");
+		}
+
+		// update
+		double t0 = t;
+
+		t = (1 + FastMath.sqrt(1 + 4 * t0 * t0))/2;
+//		System.out.println("Y");
+		for (int i = 0; i < y.length; i ++) {
+
+			y[i] = x[i] + (t0 - 1) / t * (x[i] - x0[i]);
+
+//			if (i % imgWidth == 0) {
+//				System.out.println();
+//			}
+//			System.out.print(y[i]);
+//			System.out.print(",");
+
+		}
+	}
+}
--- src/edu/uchc/octanecore/test/FittingTest.java
+++ src/edu/uchc/octanecore/test/FittingTest.java
@@ -1,13 +1,18 @@
 package edu.uchc.octanecore.test;
 
-import org.junit.Test;
 import static org.junit.Assert.assertArrayEquals;
 
+import org.junit.Ignore;
+import org.junit.Test;
+
 import edu.uchc.octanecore.datasource.RectangularImage;
-import edu.uchc.octanecore.fitting.*;
+import edu.uchc.octanecore.fitting.IterativeShrinkageAndThreshold;
+import edu.uchc.octanecore.fitting.LeastSquareGaussianFitting;
+import edu.uchc.octanecore.fitting.LeastSquareIntegratedGaussianFitting;
+import edu.uchc.octanecore.fitting.RadialSymmetryFitting;
 
 public class FittingTest {
-	
+
 	static final int IMAGE_SIZE = 11;
 
 	static final double[] TEST_VALUES = {
@@ -21,14 +26,16 @@ public class FittingTest {
 			1.2807444490144886E-5, 1.3603475690306496E-4, 9.385191163983373E-4, 0.004210097510616101, 0.01229102844461037, 0.02336878049655781, 0.02894874699531102, 0.02336878049655781, 0.01229102844461037, 0.004210097510616101, 9.385191163983373E-4,
 			4.3869876640759965E-6, 4.6596555689039867E-5, 3.2147488824231735E-4, 0.00144210235366173, 0.004210097510616101, 0.008004606371066857, 0.00991593569322976, 0.008004606371066857, 0.004210097510616101, 0.00144210235366173, 3.2147488824231735E-4,
 			9.779516450051282E-7, 1.0387350450247402E-5, 7.166350121265529E-5, 3.2147488824231735E-4, 9.385191163983373E-4, 0.0017843947983501222, 0.002210470228208766, 0.0017843947983501222, 9.385191163983373E-4, 3.2147488824231735E-4, 7.166350121265529E-5,
-			1.4175035112951352E-7, 1.5056067251874795E-6, 1.0387350450247402E-5, 4.6596555689039867E-5, 1.3603475690306496E-4, 2.5864120226361595E-4, 3.203992064538993E-4, 2.5864120226361595E-4, 1.3603475690306496E-4, 4.6596555689039867E-5, 1.0387350450247402E-5}; 
+			1.4175035112951352E-7, 1.5056067251874795E-6, 1.0387350450247402E-5, 4.6596555689039867E-5, 1.3603475690306496E-4, 2.5864120226361595E-4, 3.203992064538993E-4, 2.5864120226361595E-4, 1.3603475690306496E-4, 4.6596555689039867E-5, 1.0387350450247402E-5};
+
+	static final double[] TEST_VALUES_WITH_BACKGROUND = new double [TEST_VALUES.length];
 
 	static final double [] START_PARAMS = {5.5, 5.5, 1, 1.5, 0.8};
-	
+
 	static {
-		for (int i = 0; i < TEST_VALUES.length; i ++) {
-			TEST_VALUES[i] += 1.0;
-		}		
+		for (int i = 0; i < TEST_VALUES_WITH_BACKGROUND.length; i ++) {
+			TEST_VALUES_WITH_BACKGROUND[i] = TEST_VALUES[i] + 1.0;
+		}
 	}
 
 	@Test
@@ -36,30 +43,29 @@ public class FittingTest {
 
 		//System.out.println("Least Square Guassian");
 		LeastSquareGaussianFitting fitting = new LeastSquareGaussianFitting();
-		double[] result = fitting.fit(new RectangularImage(TEST_VALUES, IMAGE_SIZE), START_PARAMS);
-		
-		double [] expected = {6, 5, 1, 1.53, 1};
+		double[] result = fitting.fit(new RectangularImage(TEST_VALUES_WITH_BACKGROUND, IMAGE_SIZE), START_PARAMS);
 
+		double [] expected = {6, 5, 1, 1.53, 1};
 		assertArrayEquals(expected, result, 0.01);
 	}
 
 	@Test
 	public void testLeastSquareGaussinFixedSigma() {
-	
+
 		//System.out.println("Least Square Guassian fixed sigma");
 		LeastSquareGaussianFitting fitting = new LeastSquareGaussianFitting(false, true, false, 500);
-		double [] result = fitting.fit(new RectangularImage(TEST_VALUES, IMAGE_SIZE), START_PARAMS);
-		
+		double [] result = fitting.fit(new RectangularImage(TEST_VALUES_WITH_BACKGROUND, IMAGE_SIZE), START_PARAMS);
+
 		double [] expected = {6, 5, 0.976, 1.5, 1};
 		assertArrayEquals(expected, result, 0.01);
 	}
 
 	@Test
 	public void testLeastSquareIntegratedGaussin() {
-		
+
 		//System.out.println("Least Square integrated Guassian");
 		LeastSquareIntegratedGaussianFitting fitting = new LeastSquareIntegratedGaussianFitting();
-		double [] result = fitting.fit(new RectangularImage(TEST_VALUES, IMAGE_SIZE), START_PARAMS);
+		double [] result = fitting.fit(new RectangularImage(TEST_VALUES_WITH_BACKGROUND, IMAGE_SIZE), START_PARAMS);
 
 		double [] expected = {6, 5, 1, 1.5, 1};
 		assertArrayEquals(expected, result, 0.01);
@@ -69,9 +75,26 @@ public class FittingTest {
 	public void testRadialSymmetricFitting () {
 
 		RadialSymmetryFitting fitting = new RadialSymmetryFitting();
-		double [] result = fitting.fit(new RectangularImage(TEST_VALUES, IMAGE_SIZE));
+		double [] result = fitting.fit(new RectangularImage(TEST_VALUES_WITH_BACKGROUND, IMAGE_SIZE));
 
 		double [] expected = {6, 5};
-		assertArrayEquals(expected, result, 0.01);		
+		assertArrayEquals(expected, result, 0.01);
+	}
+
+	@Ignore
+	@Test
+	public void testIsta() {
+
+		int scale = 8;
+		IterativeShrinkageAndThreshold ista = new IterativeShrinkageAndThreshold(0.03, 0.15, 2000);
+		double[] x = ista.fit(new RectangularImage(TEST_VALUES, IMAGE_SIZE), scale, 1.50);
+
+		for (int i = 0; i < x.length; i++) {
+			if (i % (IMAGE_SIZE * scale) == 0) {
+				System.out.println();
+			}
+			System.out.print(x[i]);
+			System.out.print(",");
+		}
 	}
 }
