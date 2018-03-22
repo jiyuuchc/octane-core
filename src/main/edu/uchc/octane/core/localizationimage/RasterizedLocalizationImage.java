de7c856719ab50aaaa0f6e9734027eb57494dd83
Ji Yu <jyu@uchc.edu>
2018-03-22 13:45:55 -0400

    Fix bug in RasterizedLocalizationImage. * Thread safety * Remove hard coded image size

--- src/main/edu/uchc/octane/core/localizationimage/RasterizedLocalizationImage.java
+++ src/main/edu/uchc/octane/core/localizationimage/RasterizedLocalizationImage.java
@@ -7,64 +7,56 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.commons.math3.util.FastMath;
+
 import edu.uchc.octane.core.datasource.OctaneDataFile;
 
 public class RasterizedLocalizationImage extends LocalizationImage {
 
 	private short [] pixels = null;
-	private boolean isDirty = true, isDone = false;
 	private Runnable renderingCallback = null;
 
 	private int [] cachedDataIdx = null;
 	private int [] cachedPixelIdx = null;
 
-	protected double pixelSize;
-	protected int dimx, dimy;
-	protected Rectangle roi;
-	protected HashMap<Integer, double []> filters;
+	double pixelSize;
+	int dimx, dimy;
+	Rectangle roi;
+	HashMap<Integer, double []> filters;
 
-
-	private RenderingThread renderingThread = null;
+	RenderingThread renderingThread = null;
 
 	class RenderingThread extends Thread {
+	    boolean isDone;
 
 		@Override
 		public void run() {
-			isDirty = false;
-			isDone = false;
-			setPriority(Thread.MAX_PRIORITY);
+		    isDone = false;
 
-			doRendering();
-		}
+			// setPriority(Thread.MAX_PRIORITY);
 
-		public void doRendering() {
-
-			if (isDirty || interrupted()) {
-				return;
-			}
 			Arrays.fill(pixels, (short)0);
+			
 			for (int i = 0 ; i < cachedDataIdx.length; i ++) {
 
-				if (isDirty || interrupted()) {
+			    if (interrupted()) {
 					return;
 				}
 
-				int idx = cachedDataIdx[i];
-				int idx2 = cachedPixelIdx[i];
 				boolean filteredOut = false;
-				synchronized(filters) {
-					for (Map.Entry<Integer, double[]> entry : filters.entrySet()) {
-						int col = entry.getKey();
-						double [] r = entry.getValue();
-						if (data[col][idx] < r[0] || data[col][idx] > r[1]) {
-							filteredOut = true;
-							break;
-						}
-					}
+
+				for (Map.Entry<Integer, double[]> entry : filters.entrySet()) {
+				    int col = entry.getKey();
+				    double [] r = entry.getValue();
+				    if (data[col][cachedDataIdx[i]] < r[0] || data[col][cachedDataIdx[i]] > r[1]) {
+				        filteredOut = true;
+				        break;
+				    }
 				}
 				if (! filteredOut) {
-					pixels[idx2] ++;
-				}
+				    pixels[cachedPixelIdx[i]] ++;
+				}					
+			
 			}
 
 			isDone = true;
@@ -76,7 +68,18 @@ public class RasterizedLocalizationImage extends LocalizationImage {
 	}
 
 	public RasterizedLocalizationImage(OctaneDataFile locData) {
-		this(locData, 16.0, 5120, 5120);
+		this(locData, 16.0);
+	}
+
+	public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize) {
+	    super(locData);
+	    this.pixelSize = pixelSize;
+	    int maxX = (int) FastMath.floor(getSummaryStatitics(xCol).getMax() / pixelSize) + 1;
+	    int maxY = (int) FastMath.floor(getSummaryStatitics(yCol).getMax() / pixelSize) + 1;
+	    dimx = findPreferredRasterSize(maxX);
+	    dimy = findPreferredRasterSize(maxY);
+        setRoi(null);
+        filters = new HashMap<Integer, double[]>();	    
 	}
 
 	public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize, int dimX, int dimY) {
@@ -92,39 +95,38 @@ public class RasterizedLocalizationImage extends LocalizationImage {
 		this((OctaneDataFile) s.readObject());
 	}
 
-	public HashMap<Integer, double[] > getViewFilters() {
+	int findPreferredRasterSize(int d) {
+	    if (d > 10240) {
+	        return 10 * ((int) FastMath.floor(d / 10.0) + 1);
+	    }
+        //prefer 2^n * 10;
+	    int pd = 32;
+	    while (pd * 10 < d) {
+	        pd *= 2;
+	    }
+	    return pd * 10;
+	}
+
+    public int getDimX() {
+        return dimx;
+    }
+
+    public int getDimY() {
+        return dimy;
+    }
+
+    public double getPixelSize() {
+        return pixelSize;
+    }
+
+	public HashMap<Integer, double[]> getViewFilters() {
 		return filters;
 	}
 
 	public void addViewFilter(int col, double[] v) {
-
-		isDirty = true;
-		renderingThread = null;
-		synchronized(filters) {
-			filters.put(col, v);
-		}
-		// restartRendering();
-	}
-
-//	public void setDim(int x, int y) {
-//		dimx = x;
-//		dimy = y;
-//	}
-
-	public int getDimX() {
-		return dimx;
-	}
-
-	public int getDimY() {
-		return dimy;
-	}
-
-//	public void setPixelSize(double s) {
-//		pixelSize = s;
-//	}
-
-	public double getPixelSize() {
-		return pixelSize;
+	    
+	    quitCurrentRendering();
+	    filters.put(col, v);
 	}
 
 	public void setRoi(Rectangle rect) {
@@ -137,12 +139,11 @@ public class RasterizedLocalizationImage extends LocalizationImage {
 			return;
 		}
 
-		isDirty = true;
 		roi = rect;
-
+		quitCurrentRendering();
+		
 		int [] tmpIdx1 = new int[getNumLocalizations()];
 		int [] tmpIdx2 = new int[getNumLocalizations()];
-		renderingThread = null;
 
 		pixels = new short[rect.width * rect.height];
 
@@ -157,46 +158,23 @@ public class RasterizedLocalizationImage extends LocalizationImage {
 				cnt++;
 			}
 		}
+
 		cachedDataIdx = new int[cnt];
 		cachedPixelIdx = new int[cnt];
 		System.arraycopy(tmpIdx1, 0, cachedDataIdx, 0, cnt);
 		System.arraycopy(tmpIdx2, 0, cachedPixelIdx, 0, cnt);
-
-		//restartRendering();
 	}
 
 	public Rectangle getRoi() {
 		return roi;
 	}
 
-	public boolean isRenderingDone() {
-		return isDone;
-	}
-
-	public void render(short [] pixels) {
-
-		Rectangle rect = roi;
-		if (rect == null) {
-			rect = new Rectangle(0,0,getDimX(), getDimY());
-		}
-		assert(pixels.length == rect.width * rect.height);
-		for (int i = 0 ; i < getNumLocalizations(); i ++) {
-
-			int x = (int)(getXAt(i) / pixelSize);
-			int y = (int)(getYAt(i) / pixelSize);
-			if (rect.contains(x, y)) {
-				int idx = (y - rect.y) * rect.width + x - rect.x;
-				pixels[idx] ++;
-			}
-		}
-	}
-
 	public short [] getRendered() {
-		if (renderingThread == null) {
+		if ( renderingThread == null ) {
 			restartRendering();
 		}
 
-		while (! isDone ) {
+		while (! renderingThread.isDone) {
 			try {
 				renderingThread.join();
 			} catch (InterruptedException e) {
@@ -207,20 +185,28 @@ public class RasterizedLocalizationImage extends LocalizationImage {
 	}
 
 	public void restartRendering() {
-
-		if (renderingThread != null && renderingThread.isAlive()) {
-			renderingThread.interrupt();
-			try {
-				renderingThread.join();
-			} catch (InterruptedException e) {
-				;
-			}
-		}
 		renderingThread = new RenderingThread();
 		renderingThread.start();
 	}
 
 	public void onRenderingDone(Runnable callback) {
 		renderingCallback = callback;
+	}
+	
+	public void interruptRenderingThread() {
+	    if (renderingThread != null ) {
+	        renderingThread.interrupt();
+	    }
+	}
+	
+	public void quitCurrentRendering() {
+	    if (renderingThread != null ) {
+            renderingThread.interrupt();	
+            try {
+                renderingThread.join();
+            } catch (InterruptedException e) {
+            }	    
+	    }
+	    renderingThread = null;
 	}
 }
