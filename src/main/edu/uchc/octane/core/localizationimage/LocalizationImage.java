41e4031d9261bb145a672753f2ae063f107851d1
Ji Yu <jyu@uchc.edu>
2018-03-26 15:07:18 -0400

    Bug fix

--- src/main/edu/uchc/octane/core/localizationimage/LocalizationImage.java
+++ src/main/edu/uchc/octane/core/localizationimage/LocalizationImage.java
@@ -13,200 +13,199 @@ import edu.uchc.octane.core.utils.HDataCollection;
 
 public class LocalizationImage {
 
-	final Logger logger = LoggerFactory.getLogger(getClass());
+    final Logger logger = LoggerFactory.getLogger(getClass());
 
-	final OctaneDataFile dataSrc;
-	SummaryStatistics [] stats = null;
-	HashMap<String, Integer> headersMap = null;
+    final OctaneDataFile dataSrc;
+    SummaryStatistics[] stats = null;
+    HashMap<String, Integer> headersMap = null;
     FastKDTree tree;
 
-	public final double [][]data;
-	public int xCol = 1, yCol = 2, zCol = -1, frameCol = 0, errCol = -1, intensityCol = -1, sigmaCol = -1, densityCol = -1;
+    public final double[][] data;
+    public int xCol = 1, yCol = 2, zCol = -1, frameCol = 0, errCol = -1, intensityCol = -1, sigmaCol = -1,
+            densityCol = -1;
 
-	class HDataImp implements HDataCollection {
+    class HDataImp implements HDataCollection {
 
-		private double[][] data_;
-		private int d = 0;
+        private double[][] data_;
+        private int d = 0;
 
-		public HDataImp() {
-			data_ = new double[2][];
-			data_[0] = data[xCol];
-			data_[1] = data[yCol];
-		}
+        public HDataImp() {
+            data_ = new double[2][];
+            data_[0] = data[xCol];
+            data_[1] = data[yCol];
+        }
 
-		@Override
-		public int getDimension() {
-			return 2;
-		}
+        @Override
+        public int getDimension() {
+            return 2;
+        }
 
-		@Override
-		public int size() {
-			return getNumLocalizations();
-		}
+        @Override
+        public int size() {
+            return getNumLocalizations();
+        }
 
-		@Override
-		public double get(int idx, int d) {
-			return data_[d][idx];
-		}
+        @Override
+        public double get(int idx, int d) {
+            return data_[d][idx];
+        }
 
-		@Override
-		public void selectDimension(int d) {
-			this.d = d;
+        @Override
+        public void selectDimension(int d) {
+            this.d = d;
 
-		}
+        }
 
-		@Override
-		public double get(int idx) {
-			return get(idx, d);
-		}
+        @Override
+        public double get(int idx) {
+            return get(idx, d);
+        }
 
-	}
+    }
 
-	public LocalizationImage(OctaneDataFile raw) {
-		dataSrc = raw;
-		data = new double[raw.data.length + 1][]; // last column is for storing local density
-		System.arraycopy(raw.data, 0, data, 0, raw.data.length);
+    public LocalizationImage(OctaneDataFile raw) {
+        dataSrc = raw;
+        data = new double[raw.data.length + 1][]; // last column is for storing local density
+        System.arraycopy(raw.data, 0, data, 0, raw.data.length);
 
-		stats = new SummaryStatistics[data.length];
+        stats = new SummaryStatistics[data.length];
 
-		if (headersMap == null) {
-            headersMap = new HashMap<String, Integer> ();
+        if (headersMap == null) {
+            headersMap = new HashMap<String, Integer>();
             for (int i = 0; i < dataSrc.headers.length; i++) {
                 headersMap.put(dataSrc.headers[i], i);
             }
-            headersMap.put("LocalDensity", data.length - 1);            
+            headersMap.put("LocalDensity", data.length - 1);
         }
-		
-		guessHeaders();
-	}
-	
-	public LocalizationImage(LocalizationImage loc) {
-		this(new OctaneDataFile(loc.dataSrc));
-		for (int i = 0; i < stats.length; i++) {
-		    if (loc.stats[i] != null) {
-		        stats[i] = new SummaryStatistics(loc.stats[i]);
-		    }
-		}
-	}
 
-	void guessHeaders() {
-		for (String key:headersMap.keySet()) {
-			int col = headersMap.get(key);
-			key = key.toLowerCase();
-			if (key.startsWith("x ") || key.equals("x")) {
-				xCol = col;
-			} else if (key.startsWith("y ") || key.equals("y")) {
-				yCol = col;
-			} else if (key.startsWith("z ") || key.equals("z")) {
-				zCol = col;
-			} else if (key.startsWith("uncertain") || key.startsWith("err")) {
-				errCol = col;
-			} else if (key.startsWith("int")) {
-				intensityCol = col;
-			} else if (key.startsWith("frame")) {
-				frameCol = col;
-			} else if (key.startsWith("sigma")) {
-				sigmaCol = col;
-			}
-		}
+        guessHeaders();
+    }
 
-	}
-	
-	public SummaryStatistics getSummaryStatistics(int col) {
+    public LocalizationImage(LocalizationImage loc) {
+        this(new OctaneDataFile(loc.dataSrc));
+        for (int i = 0; i < stats.length; i++) {
+            if (loc.stats[i] != null) {
+                stats[i] = new SummaryStatistics(loc.stats[i]);
+            }
+        }
+    }
 
-	    if (col < 0 || col >= data.length) {
-	        return null;
-	    }
-		if ( stats[col] == null) {
-		    stats[col] = new SummaryStatistics();
-		    for (double d : data[col]) {
-		        stats[col].addValue(d);
-		    }
-		}
-		return stats[col];
-	}
+    void guessHeaders() {
+        for (String key : headersMap.keySet()) {
+            int col = headersMap.get(key);
+            key = key.toLowerCase();
+            if (key.startsWith("x ") || key.equals("x")) {
+                xCol = col;
+            } else if (key.startsWith("y ") || key.equals("y")) {
+                yCol = col;
+            } else if (key.startsWith("z ") || key.equals("z")) {
+                zCol = col;
+            } else if (key.startsWith("uncertain") || key.startsWith("err")) {
+                errCol = col;
+            } else if (key.startsWith("int")) {
+                intensityCol = col;
+            } else if (key.startsWith("frame")) {
+                frameCol = col;
+            } else if (key.startsWith("sigma")) {
+                sigmaCol = col;
+            }
+        }
+    }
 
-	public SummaryStatistics getSummaryStatistics(String header) {
-	    Integer col = getColFromHeader(header);
-		if (col != null ) {
-			return getSummaryStatistics(col);
-		} else {
-			return null;
-		}
-	}
+    public SummaryStatistics getSummaryStatistics(int col) {
 
-	public double [] getData(int col) {
-		return data[col];
-	}
+        if (col < 0 || col >= data.length) {
+            return null;
+        }
+        if (stats[col] == null) {
+            stats[col] = new SummaryStatistics();
+            for (double d : data[col]) {
+                stats[col].addValue(d);
+            }
+        }
+        return stats[col];
+    }
 
-	public double [] getData(String colHeader) {
-		return getData(getColFromHeader(colHeader));
-	}
+    public SummaryStatistics getSummaryStatistics(String header) {
+        Integer col = getColFromHeader(header);
+        if (col != null) {
+            return getSummaryStatistics(col);
+        } else {
+            return null;
+        }
+    }
 
-	public String[] getHeaders() {
-		return dataSrc.headers;
-	}
+    public double[] getData(int col) {
+        return data[col];
+    }
 
-	public int getNumLocalizations() {
-		return data[frameCol].length;
-	}
+    public double[] getData(String colHeader) {
+        return getData(getColFromHeader(colHeader));
+    }
 
-	public double getXAt(int idx) {
-		return data[xCol][idx];
-	}
+    public String[] getHeaders() {
+        return dataSrc.headers;
+    }
 
-	public double getYAt(int idx) {
-		return data[yCol][idx];
-	}
+    public int getNumLocalizations() {
+        return data[frameCol].length;
+    }
 
-	public double getZAt(int idx) {
-		return data[zCol][idx];
-	}
+    public double getXAt(int idx) {
+        return data[xCol][idx];
+    }
 
-	public void constructKDtree() {
-		tree = new FastKDTree(new HDataImp());
-	}
+    public double getYAt(int idx) {
+        return data[yCol][idx];
+    }
 
-	public void measureLocalDensity(double distance) {
-		assert(tree != null);
-		int localDensityCol = data.length -1 ;
-		for (int i = 0; i < getNumLocalizations(); i++) {
-			data[localDensityCol][i] = tree.radiusSearch(i, distance).size();
-		}
-		densityCol = localDensityCol;
-	}
-	
+    public double getZAt(int idx) {
+        return data[zCol][idx];
+    }
+
+    public void constructKDtree() {
+        tree = new FastKDTree(new HDataImp());
+    }
+
+    public void measureLocalDensity(double distance) {
+        assert (tree != null);
+        int localDensityCol = data.length - 1;
+        for (int i = 0; i < getNumLocalizations(); i++) {
+            data[localDensityCol][i] = tree.radiusSearch(i, distance).size();
+        }
+        densityCol = localDensityCol;
+    }
+
     public String getHeader(int i) {
         return dataSrc.headers[i];
     }
-    
+
     public Integer getColFromHeader(String s) {
-        
         return headersMap.get(s);
     }
 
-    //in default axis convention:
-	// rotation around top left corner (0,0)
-	// positive theta values rotate counter-clock-wise
-	public void rotate(double theta) {
-		double [] xData = getData(xCol);
-		double [] yData = getData(yCol);
-		double cosTheta = FastMath.cos(theta);
-		double sinTheta = FastMath.sin(theta);
-		for (int i = 0; i < getNumLocalizations(); i ++) {
-			double x = cosTheta * xData[i] - sinTheta * yData[i];
-			double y = sinTheta * xData[i] + cosTheta * yData[i];
-			xData[i] = x;
-			yData[i] = y;
-		}
-	}
-	
-	public void translate(double dx, double dy) {
-        double [] xData = getData(xCol);
-        double [] yData = getData(yCol);
-        for (int i = 0; i < getNumLocalizations(); i ++) {
+    // in default axis convention:
+    // rotation around top left corner (0,0)
+    // positive theta values rotate counter-clock-wise
+    public void rotate(double theta) {
+        double[] xData = getData(xCol);
+        double[] yData = getData(yCol);
+        double cosTheta = FastMath.cos(theta);
+        double sinTheta = FastMath.sin(theta);
+        for (int i = 0; i < getNumLocalizations(); i++) {
+            double x = cosTheta * xData[i] - sinTheta * yData[i];
+            double y = sinTheta * xData[i] + cosTheta * yData[i];
+            xData[i] = x;
+            yData[i] = y;
+        }
+    }
+
+    public void translate(double dx, double dy) {
+        double[] xData = getData(xCol);
+        double[] yData = getData(yCol);
+        for (int i = 0; i < getNumLocalizations(); i++) {
             xData[i] += dx;
             yData[i] += dy;
-        }	    
-	}
+        }
+    }
 }
--- src/main/edu/uchc/octane/core/localizationimage/RasterizedLocalizationImage.java
+++ src/main/edu/uchc/octane/core/localizationimage/RasterizedLocalizationImage.java
@@ -11,121 +11,73 @@ import org.apache.commons.math3.util.FastMath;
 
 import edu.uchc.octane.core.datasource.OctaneDataFile;
 
-public class RasterizedLocalizationImage extends LocalizationImage {
+public class RasterizedLocalizationImage extends LocalizationImage implements Runnable {
 
-	private short [] pixels = null;
-	private Runnable renderingCallback = null;
+    private short[] pixels = null;
+    private Runnable renderingCallback = null;
 
-	private int [] cachedDataIdx = null;
-	private int [] cachedPixelIdx = null;
+    private int[] cachedDataIdx = null;
+    private int[] cachedPixelIdx = null;
 
-	double pixelSize;
-	int dimx, dimy;
-	Rectangle roi;
-	HashMap<Integer, double []> filters;
+    double pixelSize;
+    int dimx, dimy;
+    Rectangle roi;
+    HashMap<Integer, double[]> filters;
 
-	RenderingThread renderingThread = null;
+    Thread renderingThread = null;
+    boolean isDone;
 
-	class RenderingThread extends Thread {
-	    boolean isDone;
+    public RasterizedLocalizationImage(OctaneDataFile locData) {
+        this(locData, 16.0);
+    }
 
-		@Override
-		public void run() {
-		    isDone = false;
-
-			// setPriority(Thread.MAX_PRIORITY);
-
-			Arrays.fill(pixels, (short)0);
-			
-			for (int i = 0 ; i < cachedDataIdx.length; i ++) {
-
-			    if (interrupted()) {
-					return;
-				}
-
-				boolean filteredOut = false;
-
-				synchronized(filters) {
-				for (Map.Entry<Integer, double[]> entry : filters.entrySet()) {
-				    int col = entry.getKey();
-				    double [] r = entry.getValue();
-				    int c = cachedDataIdx[i];
-				    double [] vc = data[col];
-//				    if (vc==null) {
-//				        System.out.println(col);
-//				    }
-				    double v = vc[c];
-				    if ( v < r[0] || v > r[1]) {
-				        filteredOut = true;
-				        break;
-				    }
-				}
-				if (! filteredOut) {
-				    pixels[cachedPixelIdx[i]] ++;
-				}					
-			
-				}
-			}
-
-			isDone = true;
-
-			if (renderingCallback != null) {
-				renderingCallback.run();
-			}
-		}
-	}
-
-	public RasterizedLocalizationImage(OctaneDataFile locData) {
-		this(locData, 16.0);
-	}
-
-	public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize) {
-	    super(locData);
-	    this.pixelSize = pixelSize;
-	    int maxX = (int) FastMath.floor(getSummaryStatistics(xCol).getMax() / pixelSize);
-	    int maxY = (int) FastMath.floor(getSummaryStatistics(yCol).getMax() / pixelSize);
-	    dimx = findPreferredRasterSize(maxX);
-	    dimy = findPreferredRasterSize(maxY);
+    public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize) {
+        super(locData);
+        this.pixelSize = pixelSize;
+        int maxX = (int) FastMath.floor(getSummaryStatistics(xCol).getMax() / pixelSize);
+        int maxY = (int) FastMath.floor(getSummaryStatistics(yCol).getMax() / pixelSize);
+        dimx = findPreferredRasterSize(maxX);
+        dimy = findPreferredRasterSize(maxY);
         setRoi(null);
-        filters = new HashMap<Integer, double[]>();	    
-	}
+        filters = new HashMap<Integer, double[]>();
+    }
 
-	public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize, int dimX, int dimY) {
-		super(locData);
-		this.pixelSize = pixelSize;
-		this.dimx = dimX;
-		this.dimy = dimY;
-		setRoi(null);
-		filters = new HashMap<Integer, double[]>();
-	}
+    public RasterizedLocalizationImage(OctaneDataFile locData, double pixelSize, int dimX, int dimY) {
+        super(locData);
+        this.pixelSize = pixelSize;
+        this.dimx = dimX;
+        this.dimy = dimY;
+        setRoi(null);
+        filters = new HashMap<Integer, double[]>();
+    }
 
-	public RasterizedLocalizationImage(ObjectInputStream s) throws ClassNotFoundException, IOException {
-		this((OctaneDataFile) s.readObject());
-	}
+    public RasterizedLocalizationImage(ObjectInputStream s) throws ClassNotFoundException, IOException {
+        this((OctaneDataFile) s.readObject());
+    }
 
-	public RasterizedLocalizationImage(RasterizedLocalizationImage orig) {
-	    super(orig);
-	    pixelSize = orig.pixelSize;
-	    dimx = orig.dimx;
-	    dimy = orig.dimy;
-	    setRoi(orig.roi);
-	    filters = new HashMap<Integer, double[]>();
-	    for (Integer key : orig.filters.keySet()) {
-	        filters.put(key, orig.filters.get(key).clone());
-	    }
-	}
+    public RasterizedLocalizationImage(RasterizedLocalizationImage orig) {
+        super(orig);
+        pixelSize = orig.pixelSize;
+        dimx = orig.dimx;
+        dimy = orig.dimy;
+        setRoi(orig.roi);
+        filters = new HashMap<Integer, double[]>();
+        for (Integer key : orig.filters.keySet()) {
+            filters.put(key, orig.filters.get(key).clone());
+        }
+    }
 
-	int findPreferredRasterSize(int d) {
-	    if (d > 5120) {
-	        return 10 * ((int) FastMath.floor(d / 10.0) + 1);
-	    }
-        //prefer 2^n * 10;
-	    int pd = 32;
-	    while (pd * 10 < d) {
-	        pd *= 2;
-	    }
-	    return pd * 10;
-	}
+    int findPreferredRasterSize(int d) {
+        if (d > 5120) {
+            return 10 * ((int) FastMath.floor(d / 10.0) + 1);
+        }
+        // prefer 2^n * 10;
+        int pd = 32;
+        while (pd * 10 < d) {
+            pd *= 2;
+        }
+        return pd * 10;
+    }
 
     public int getDimX() {
         return dimx;
@@ -139,96 +91,159 @@ public class RasterizedLocalizationImage extends LocalizationImage {
         return pixelSize;
     }
 
-	public HashMap<Integer, double[]> getViewFilters() {
-		return filters;
-	}
+    public HashMap<Integer, double[]> getViewFilters() {
+        return filters;
+    }
 
-	public void addViewFilter(int col, double[] v) {
-	    
-	    quitCurrentRendering();
-	    synchronized (filters) {
-	    filters.put(col, v);
-	    }
-	}
+    public synchronized void addViewFilter(int col, double[] v) {
 
-	public void setRoi(Rectangle rect) {
+        quitCurrentRendering();
+        filters.put(col, v);
+    }
 
-		if (rect == null) {
-			rect = new Rectangle(dimx, dimy);
-		}
+    public synchronized void setRoi(Rectangle rect) {
 
-		if (rect.equals(roi)) {
-			return;
-		}
+        if (rect == null) {
+            rect = new Rectangle(dimx, dimy);
+        }
 
-		roi = rect;
-		quitCurrentRendering();
-		
-		int [] tmpIdx1 = new int[getNumLocalizations()];
-		int [] tmpIdx2 = new int[getNumLocalizations()];
+        if (rect.equals(roi)) {
+            return;
+        }
 
-		pixels = new short[rect.width * rect.height];
+        roi = rect;
+        quitCurrentRendering();
 
-		int cnt = 0;
-		for (int i = 0 ; i < getNumLocalizations(); i ++) {
+        cachedDataIdx = null;
+        cachedPixelIdx = null;
+        pixels = null;
+    }
 
-			int x = (int)(getXAt(i) / pixelSize);
-			int y = (int)(getYAt(i) / pixelSize);
-			if (rect.contains(x, y)) {
-				tmpIdx1[cnt] = i;
-				tmpIdx2[cnt] = x - rect.x + (y - rect.y) * rect.width;
-				cnt++;
-			}
-		}
+    public Rectangle getRoi() {
+        return roi;
+    }
 
-		cachedDataIdx = new int[cnt];
-		cachedPixelIdx = new int[cnt];
-		System.arraycopy(tmpIdx1, 0, cachedDataIdx, 0, cnt);
-		System.arraycopy(tmpIdx2, 0, cachedPixelIdx, 0, cnt);
-	}
+    public short[] getRendered() {
+        if (renderingThread == null) {
+            startRendering();
+        }
 
-	public Rectangle getRoi() {
-		return roi;
-	}
+        try {
+            renderingThread.join();
+        } catch (InterruptedException e) {
+            return null;
+        }
 
-	public short [] getRendered() {
-		if ( renderingThread == null ) {
-			restartRendering();
-		}
+        return pixels;
+    }
 
-		while (! renderingThread.isDone) {
-			try {
-				renderingThread.join();
-			} catch (InterruptedException e) {
-			}
-		}
+    public synchronized void startRendering() {
+        renderingThread = new Thread(this);
+        renderingThread.start();
+    }
 
-		return pixels;
-	}
+    public void onRenderingDone(Runnable callback) {
+        renderingCallback = callback;
+    }
 
-	public void restartRendering() {
-		renderingThread = new RenderingThread();
-		renderingThread.start();
-	}
+    public void interruptRenderingThread() {
+        if (renderingThread != null) {
+            renderingThread.interrupt();
+        }
+    }
 
-	public void onRenderingDone(Runnable callback) {
-		renderingCallback = callback;
-	}
-	
-	public void interruptRenderingThread() {
-	    if (renderingThread != null ) {
-	        renderingThread.interrupt();
-	    }
-	}
-	
-	public void quitCurrentRendering() {
-	    if (renderingThread != null ) {
-            renderingThread.interrupt();	
+    public void quitCurrentRendering() {
+        if (renderingThread != null) {
+            renderingThread.interrupt();
             try {
                 renderingThread.join();
             } catch (InterruptedException e) {
-            }	    
-	    }
-	    renderingThread = null;
-	}
+            }
+        }
+        renderingThread = null;
+    }
+
+    @Override
+    public synchronized void rotate(double theta) {
+        quitCurrentRendering();
+        super.rotate(theta);
+
+        cachedDataIdx = null;
+        cachedPixelIdx = null;
+    }
+
+    @Override
+    public synchronized void translate(double dx, double dy) {
+        quitCurrentRendering();
+        super.translate(dx, dy);
+
+        cachedDataIdx = null;
+        cachedPixelIdx = null;
+    }
+
+    void cachePositions() {
+        int[] tmpIdx1 = new int[getNumLocalizations()];
+        int[] tmpIdx2 = new int[getNumLocalizations()];
+
+        int cnt = 0;
+        for (int i = 0; i < getNumLocalizations(); i++) {
+
+            int x = (int) (getXAt(i) / pixelSize);
+            int y = (int) (getYAt(i) / pixelSize);
+            if (roi.contains(x, y)) {
+                tmpIdx1[cnt] = i;
+                tmpIdx2[cnt] = x - roi.x + (y - roi.y) * roi.width;
+                cnt++;
+            }
+        }
+
+        cachedDataIdx = new int[cnt];
+        cachedPixelIdx = new int[cnt];
+        System.arraycopy(tmpIdx1, 0, cachedDataIdx, 0, cnt);
+        System.arraycopy(tmpIdx2, 0, cachedPixelIdx, 0, cnt);
+    }
+
+    @Override
+    public void run() {
+
+        isDone = false;
+
+        if (cachedDataIdx == null) {
+            cachePositions();
+        }
+        if (pixels == null) {
+            pixels = new short[roi.width * roi.height];
+        }
+        Arrays.fill(pixels, (short) 0);
+
+        for (int i = 0; i < cachedDataIdx.length; i++) {
+
+            if (Thread.interrupted()) {
+                return;
+            }
+
+            boolean filteredOut = false;
+
+            for (Map.Entry<Integer, double[]> entry : filters.entrySet()) {
+                int col = entry.getKey();
+                double[] r = entry.getValue();
+                int c = cachedDataIdx[i];
+                double[] vc = data[col];
+                double v = vc[c];
+                if (v < r[0] || v > r[1]) {
+                    filteredOut = true;
+                    break;
+                }
+            }
+            if (!filteredOut) {
+                pixels[cachedPixelIdx[i]]++;
+            }
+
+        }
+        isDone = true;
+
+        if (renderingCallback != null) {
+            renderingCallback.run();
+        }
+    }
 }
