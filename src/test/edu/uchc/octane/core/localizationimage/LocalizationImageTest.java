e023a7a515397b6037f95679d671e32ea588ede7
Ji Yu <jyu@uchc.edu>
2018-03-28 09:00:18 -0400

    Add testing for rotation

---
+++ src/test/edu/uchc/octane/core/localizationimage/LocalizationImageTest.java
@@ -0,0 +1,27 @@
+package edu.uchc.octane.core.localizationimage;
+
+import static org.junit.Assert.assertArrayEquals;
+
+import org.apache.commons.math3.util.FastMath;
+import org.junit.Test;
+
+import edu.uchc.octane.core.datasource.OctaneDataFile;
+
+public class LocalizationImageTest {
+
+    @Test
+    public void testRotation() {
+        String [] header = {"x", "y"};
+        double [][] data = {
+                {1,2,3},
+                {4,5,6}
+        };
+
+        LocalizationImage img = new LocalizationImage(new OctaneDataFile(data, header));
+        double theta = -FastMath.PI / 2; 
+        img.rotate(theta, 0, 0);
+        
+        double [] expected = {4,5,6};
+        assertArrayEquals(data[0], expected, 0.001);
+    }
+}
