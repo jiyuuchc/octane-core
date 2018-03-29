392d48a15549281d0e259e94dcc01703e34c2bcf
Ji Yu <jyu@uchc.edu>
2018-03-29 10:34:32 -0400

    Cli message now show correct version number

--- src/main/edu/uchc/octane/core/cli/Main.java
+++ src/main/edu/uchc/octane/core/cli/Main.java
@@ -1,39 +1,54 @@
 package edu.uchc.octane.core.cli;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Properties;
+
+import org.json.JSONException;
 
 public class Main {
 
-	public static void printHelp() {
-		System.out.println("Octane v0.1 - available commands");
-		System.out.println("\tanalyze\t analyze raw image data for localizations");
-		System.out.println("\tdrift\t correct drift in the localzation data");
-		System.out.println("\timport\t import localization data from Thunderstorm output");
-		System.out.println("\tmerge\t track trajectories and merge coordinantes" );
-		System.out.println();
-		System.out.println("octane cmd -h for further help");
-	}
+    final static Properties properties = new Properties();
 
-	public static void main(String[] args) throws Exception {
+    public static void printHelp() {
+        System.out.println("Octane(cli) v" + properties.getProperty("version") + " - available commands");
+        System.out.println("\tanalyze\t analyze raw image data for localizations");
+        System.out.println("\tdrift\t correct drift in the localzation data");
+        System.out.println("\timport\t import localization data from Thunderstorm output");
+        System.out.println("\tmerge\t track trajectories and merge coordinantes");
+        System.out.println();
+        System.out.println("octane cmd -h for further help");
+    }
 
-		String cmd = args[0];
-		args = Arrays.copyOfRange(args, 1, args.length);
+    public static void main(String[] args) throws IOException, JSONException, ClassNotFoundException {
 
-		if (cmd.equals("about") ){
-			System.out.println("octane 0.1");
-			System.out.println("Single molecule localization microscopy data analysis tools");
-		} else if (cmd.equals("import")) {
-			ImportCommand.run(args);
-		}  else if (cmd.equals("drift")) {
-			DriftCommand.run(args);
-		} else if (cmd.equals("analyze")) {
-			AnalyzeCommand.run(args);
-		} else if (cmd.equals("merge")) {
-			TrackingCommand.run(args);
-		} else {
-			printHelp();
-		}
+        InputStream stream = Main.class.getClassLoader().getResourceAsStream("project.properties");
+        properties.load(stream);
+        
+        if (args == null || args.length == 0) {
+            printHelp();
+            return;
+        }
 
-		System.exit(0);
-	}
+        String cmd = args[0];
+        if (args.length > 1) {
+            args = Arrays.copyOfRange(args, 1, args.length);
+        }
+
+        if (cmd.equals("about")) {
+            System.out.println("Octane " + properties.getProperty("version"));
+            System.out.println("Single molecule localization microscopy data analysis tools");
+        } else if (cmd.equals("import")) {
+            ImportCommand.run(args);
+        } else if (cmd.equals("drift")) {
+            DriftCommand.run(args);
+        } else if (cmd.equals("analyze")) {
+            AnalyzeCommand.run(args);
+        } else if (cmd.equals("merge")) {
+            TrackingCommand.run(args);
+        } else {
+            printHelp();
+        }
+    }
 }
